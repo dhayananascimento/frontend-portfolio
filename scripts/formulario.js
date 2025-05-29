44ff5a88e6c8d9b14c79a47e52e8cb56411dc218
Dhayana N. Silva <dhayanadesenvolvimento@gmail.com>
2025-05-29 04:02:58 -0300

    adiciona bloqueio temporario para o formulario

---
+++ scripts/formulario.js
@@ -0,0 +1,6 @@
+const formulario = document.getElementById("formulario_contato");
+
+formulario.addEventListener("submit", (event) => {
+  event.preventDefault();
+  alert("Em desenvolvimento!");
+});
