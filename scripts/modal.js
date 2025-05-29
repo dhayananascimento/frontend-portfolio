b1310206fb65e268e9e6993c71d2a155d9133f07
Dhayana N. Silva <dhayanadesenvolvimento@gmail.com>
2025-05-29 03:07:12 -0300

    corrige modal

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,30 +1,35 @@
 const imagensProjeto = document.querySelectorAll(".projeto_imagem");
+let cardSelecionado = null;
 
 const modal = document.getElementById("modal");
 const fechaModal = document.getElementById("modal_fechar");
 
-function abrirModal(src, alt) {
+function abrirModal(imagem) {
   const imagemModal = document.getElementById("modal_imagem");
 
   modal.style.display = "flex";
-  imagemModal.src = src;
-  imagemModal.alt = alt;
+  imagemModal.src = imagem.src;
+  imagemModal.alt = imagem.alt;
+
+  fechaModal.focus();
+  cardSelecionado = imagem;
 }
 
 function fecharModal() {
   modal.style.display = "none";
+  cardSelecionado.focus();
 }
 
 imagensProjeto.forEach((imagem) => {
   imagem.tabIndex = 0;
   imagem.role = "button";
 
-  imagem.addEventListener("click", () => abrirModal(imagem.src, imagem.alt));
+  imagem.addEventListener("click", () => abrirModal(imagem));
 
   imagem.addEventListener("keydown", (event) => {
     if (event.key === "Enter" || event.key === " ") {
       event.preventDefault();
-      abrirModal(imagem.src, imagem.alt);
+      abrirModal(imagem);
     }
   });
 });
@@ -38,8 +43,15 @@ fechaModal.addEventListener("keydown", (event) => {
 });
 
 document.addEventListener("keydown", (event) => {
-  if (event.key === "Escape" && modal.style.display === "block") {
-    event.preventDefault();
-    fecharModal();
+  if (modal.style.display === "flex") {
+    if (event.key === "Escape") {
+      event.preventDefault();
+      fecharModal();
+    }
+
+    if (event.key === "Tab") {
+      event.preventDefault();
+      fechaModal.focus();
+    }
   }
 });
